18a929925474083dbf177b2e4f8d6c56ca205a52
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-12 15:54:23 +0200

    basic working log in page

--- react-project-5/src/App.jsx
+++ react-project-5/src/App.jsx
@@ -5,15 +5,15 @@ import LogIn from './components/LogIn'
 function App() {
   const [data, setData] = useState(null)
   useEffect(() => {
-      fetch("http://localhost:3000/users")
+      setTimeout(() => {fetch("http://localhost:3000/users")
       .then((res) => res.json())
-      .then((dat) => {setData(dat); console.log(data)})
+      .then((dat) => {setData(dat); console.log(data)})}, 2000)
   }, [])
 
   return (
     <>
      <h1>{!data ? "loading..." : data[0].name}</h1>
-     {/* <LogIn /> */}
+     <LogIn />
     </>
   )
 }
--- react-project-5/src/components/LogIn.jsx
+++ react-project-5/src/components/LogIn.jsx
@@ -1,12 +1,35 @@
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import App from '../App';
 
 export default function LogIn() {
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    const [data, setData] = useState(null)
+    useEffect(() => {
+      fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((dat) => {setData(dat); console.log(data)})
+  }, [])
+
+    function handelSubmit() {
+        for (let user in data) {
+            if(userName === data[user].name && password === data[user].website) {
+                alert(`hello, ${userName}`);
+                return data[user].name;
+            } else {
+                alert("name or password are incorrect")
+                return;    
+            }
+        } 
+    }
+
     return (
         <>
-            <input placeholder="userName" /> <br />
-            <input placeholder="password" /> <br />
-            <button type="submit">log in</button>
-            <Link to="/">dont have user?  click here</Link>
+            <input placeholder="userName" onChange={({target}) => setUserName(target.value)} /> <br />
+            <input placeholder="password" onChange={({target}) => setPassword(target.value)} /> <br />
+            <button type="submit" onClick={handelSubmit}>log in</button>
+            {/* <Link to="/">dont have user?  click here</Link> */}
         </>
     )
 }
