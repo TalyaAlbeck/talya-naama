f9a5271d7e65ced8b6ed5e8ef713bf411930b318
naamaWyler <naamawyler@hilma.tech>
2024-11-12 15:15:13 +0200

    not chuch

--- react-project-5/src/App.jsx
+++ react-project-5/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
 
 function App() {
+  let data
+  fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 
   return (
     <>
-     
+     <>
+     hiii
+     </>
     </>
   )
 }
