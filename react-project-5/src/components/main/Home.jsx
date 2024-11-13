1024ff2ed25363834f272ec407252ecb67535315
naamaWyler <naamawyler@hilma.tech>
2024-11-13 11:30:15 +0200

    info show/hide

--- react-project-5/src/components/main/Home.jsx
+++ react-project-5/src/components/main/Home.jsx
@@ -2,12 +2,24 @@ import { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Routes, Route, Outlet} from "react-router-dom";
 
 
+
+
 export default function Home(){
+    const [showInfo, setShowInfo] = useState(false)
 
     return(
         <>
             <h1>this is home</h1>
-            <Outlet />
-    </>
+
+            {!showInfo && 
+            <> 
+            <button onClick={()=>setShowInfo(true)} > show my info </button>
+            </>}
+
+            {showInfo&&
+            <h1>here's your info</h1>}
+            
+            {/* <Outlet /> */}
+        </>
     )
 }
