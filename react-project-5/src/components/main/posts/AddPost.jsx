df3db41215aa4226bd850aba6ea5402c0f61bf8e
naamaWyler <naamawyler@hilma.tech>
2024-11-14 14:25:39 +0200

    personal sts possible

---
+++ react-project-5/src/components/main/posts/AddPost.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export default function AddPost({ setMyPosts }) {
+  const [newPost, setNewPost] = useState("");
+  const currentUser = JSON.parse(localStorage.getItem("current User"));
+  const [postId, setPostId] = useState(100);
+  //   console.log("currentUser: ", currentUser);
+  //   console.log("currentUser id: ", currentUser.id);
+  useEffect(() => {}, []);
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userId: currentUser.id,
+        id: Math.random() * 10000000,
+        title: "post title",
+        body: newPost,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setMyPosts((prev) => {
+          return [...prev, res];
+        });
+      });
+
+    console.log(newPost);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="new post" onChange={({ target }) => setNewPost(target.value)} required /> <br />
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          {" "}
+          add post
+        </button>
+      </form>
+    </>
+  );
+}
